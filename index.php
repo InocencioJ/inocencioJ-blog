b21ec82512e22336088ff453fa2f3fce6e7f5447
joshua inocencio <snotbot1234@gmai.com>
2015-01-30 13:25:59 -0800

    .

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
-        <?php
-           require_once(__DIR__ . "/controller/create-db.php");
-        ?>
-    </body>
-</html>
+    <?php
+        require_once(__DIR__ . "/view/header.php");
+        require_once(__DIR__ . "/controller/create-db.php");
+        require_once(__DIR__ . "/view/footer.php");
+    ?>
